8a67387e7cf1d3f387e57ca5c225821f67a60561
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2024-12-26 13:31:56 -0300

    update README.md

--- src/cli/config.rs
+++ src/cli/config.rs
@@ -4,7 +4,6 @@ pub fn parse_modules() -> Vec<StyledModule> {
     vec![
         Some(StyledModule::new(Module::Cpu(2), Some("CPU"))),
         Some(StyledModule::new(Module::Memory(2), Some("MEM"))),
-        Some(StyledModule::new(Module::Battery, Some("BAT"))),
         Some(StyledModule::new(Module::Time("%a %d %b %R"), None)),
     ]
     .into_iter()
